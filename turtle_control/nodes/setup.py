d883e679fc0492aa02476bd2b89a8c3269bc762f
Anna Garverick <annagarverick2022@u.northwestern.edu>
2021-09-30 15:37:32 -0500

    Draws Xs according to waypoint.yaml

--- turtle_control/nodes/setup.py
+++ turtle_control/nodes/setup.py
@@ -5,6 +5,7 @@ from turtlesim.srv import Spawn
 from turtlesim.srv import Kill
 from turtlesim.srv import TeleportAbsolute
 from turtlesim.srv import TeleportRelative
+from turtlesim.srv import SetPen
 from std_srvs.srv import Empty
 
 #class Draw:
@@ -23,17 +24,33 @@ def main():
     #reset = rospy.ServiceProxy("reset",Empty)
     spawn = rospy.ServiceProxy("/spawn",Spawn)
     jump = rospy.ServiceProxy("/turtle1/teleport_absolute",TeleportAbsolute)
+    setpen = rospy.ServiceProxy("/turtle1/set_pen",SetPen)
     #spawn(0,0,0,"Turtle")
     for i in range(0,len(pts)):
         x = pts[i][0]
         y = pts[i][1]
-        jump(x+1,y+1,0)
-        #jump(x,y,0)
-        #jump(x-1,x-1,0)
-        #jump(x,y,0)
-        #jump(x-1,x+1,0)
-        #jump(x,y,0)
-        #jump(x+1,y-1,0)
+        setpen(255,0,0,2,1)
+        jump(x,y,0)
+        setpen(255,0,0,2,0)
+        jump(x+.5,y+.5,0)
+        setpen(255,0,0,2,1)
+        jump(x,y,0)
+        setpen(255,0,0,2,0)
+        jump(x-.5,y-.5,0)
+        setpen(255,0,0,2,1)
+        jump(x,y,0)
+        setpen(255,0,0,2,0)
+        jump(x-.5,y+.5,0)
+        setpen(255,0,0,2,1)
+        jump(x,y,0)
+        setpen(255,0,0,2,0)
+        jump(x+.5,y-.5,0)
+        setpen(255,0,0,2,1)
+    
+    jump(5.5,5.5,0)
+    
+
+
     #rospy.spin()
 
 if __name__ == '__main__':
