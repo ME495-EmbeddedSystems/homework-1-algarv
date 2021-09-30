3fd9f6d53ab90b5e5138a50d82ef202e5ebf9541
Anna Garverick <annagarverick2022@u.northwestern.edu>
2021-09-29 21:13:34 -0500

    Drawing X-es

--- turtle_control/nodes/setup.py
+++ turtle_control/nodes/setup.py
@@ -3,26 +3,26 @@
 import rospy
 from turtlesim.srv import Spawn
 from turtlesim.srv import Kill
+from turtlesim.srv import TeleportAbsolute as jump
 from std_srvs.srv import Empty
 
 #class Draw:
 #
-#   def __init__(self):
-#        self.pub = rospy.Publisher('cmd_vel',Twist,queue_size = 10)
-#        self.kill = rospy.ServiceProxy("kill", Kill)
-#        self.spawn = rospy.ServiceProxy("spawn", Spawn)
-#        self.draw = rospy.Service('draw',Draw,draw())
-#    
-#    def draw():
-#        self.kill("turtle1")
-#        self.spawn(0,0,0,name="turtle1")
-#
+ #   def __init__(self):
+ #       self.pub = rospy.Publisher('cmd_vel',Twist,queue_size = 10)
+ #       self.draw = rospy.Service('draw',Draw,draw())
+ #   def draw(x,y):
+ #       jump()
+
 
 def main():
     pts = rospy.get_param("/waypoints")
+    print(pts)
     rospy.init_node('draw')
     reset = rospy.ServiceProxy("reset",Empty)
     spawn = rospy.ServiceProxy("spawn",Spawn)
+    for i in range(0,len(pts)):
+        jump()
     rospy.spin()
 
 if __name__ == '__main__':
