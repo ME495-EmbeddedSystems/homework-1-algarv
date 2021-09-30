d10e72175bf44afde29b5319d280d0402ca50185
Anna Garverick <annagarverick2022@u.northwestern.edu>
2021-09-29 20:49:18 -0500

    Feed waypoints into the setup file

--- turtle_control/nodes/setup.py
+++ turtle_control/nodes/setup.py
@@ -3,25 +3,26 @@
 import rospy
 from turtlesim.srv import Spawn
 from turtlesim.srv import Kill
+from std_srvs.srv import Empty
 
-class Draw:
-
-    def __init__(self):
-        self.pub = rospy.Publisher('cmd_vel',Twist,queue_size = 10)
-        self.kill = rospy.ServiceProxy("kill", Kill)
-        self.spawn = rospy.ServiceProxy("spawn", Spawn)
-        self.draw = rospy.Service('draw',Draw,draw())
-    
-    def draw():
-        self.kill("turtle1")
-        self.spawn(0,0,0,name="turtle1")
-
+#class Draw:
+#
+#   def __init__(self):
+#        self.pub = rospy.Publisher('cmd_vel',Twist,queue_size = 10)
+#        self.kill = rospy.ServiceProxy("kill", Kill)
+#        self.spawn = rospy.ServiceProxy("spawn", Spawn)
+#        self.draw = rospy.Service('draw',Draw,draw())
+#    
+#    def draw():
+#        self.kill("turtle1")
+#        self.spawn(0,0,0,name="turtle1")
+#
 
 def main():
+    pts = rospy.get_param("/waypoints")
     rospy.init_node('draw')
     reset = rospy.ServiceProxy("reset",Empty)
-    
-    draw = Draw()
+    spawn = rospy.ServiceProxy("spawn",Spawn)
     rospy.spin()
 
 if __name__ == '__main__':
