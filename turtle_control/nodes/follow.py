a549800cd2a18b65c36a9e008762c54cde59e057
Anna Garverick <annagarverick2022@u.northwestern.edu>
2021-10-01 17:03:29 -0500

    Added a restart service in the follow node

--- turtle_control/nodes/follow.py
+++ turtle_control/nodes/follow.py
@@ -5,11 +5,11 @@ import math
 #from turtlesim.srv import TeleportAbsolute
 from turtlesim.srv import TeleportRelative
 from turtlesim.msg import Pose
+from turtlesim.srv import Spawn
 from std_srvs.srv import Empty
 
-#from Start.srv import Start
 
-dist_thresh = .5
+dist_thresh = .5 ##this should be a private parameter##
 
 def callback(data):
     rospy.loginfo(data)
@@ -27,11 +27,33 @@ def callback(data):
         theta = math.atan2((target_y-y)/(target_x-x))
         move(dist,theta)
 
+def restart(start_x, start_y):
+    spawn = rospy.ServiceProxy("turtle1/spawn",Spawn)
+    spawn(start_x,start_y,0)
+
+    pts = rospy.get_param("/waypoints")
+
+    total_distance = 0
+
+    for k in range(len(pts)):
+        if k = 0:
+            dist = math.sqrt((start_x - pts[k][0])**2 + (start_y - pts[k][1])**2)
+        else:
+            dist = math.sqrt((pts[k][0] - pts[k-1][0])**2 + (pts[k][1] - pts[k-1][1])**2)
+        total_distance = total_distance + dist
+    
+    if start_x < 0 or start_x > 10 or start_y < 0 or start_y > 10: 
+        return None
+    else:
+        return total_distance
+    
 def main():
     rospy.init_node('follow')
     rospy.wait_for_service('draw')
     draw = rospy.ServiceProxy('draw',Empty)
     draw()
+
+    rospy.Service('restart','turtle_control/Start',restart)
 
     rospy.Subscriber('/turtle1/Pose',Pose,callback)
 
--- turtle_control/nodes/translate.py
+++ turtle_control/nodes/translate.py
@@ -1,8 +1,9 @@
 import rospy
 from geometry_msgs.msg import Twist, Vector3
 from turtle_control.msg import TurtleVelocity
+from std_srvs.srv import Empty
 
-def callback(data):
+def translate(data):
     rospy.loginfo(data)
     pub = rospy.Publisher('turtle1/cmd_vel',Twist,queue_size = 10)
     twist_value = Twist(Vector3(x=data.x_velocity,y=0,z=0),Vector3(x=0,y=0,z=data.angular_velocity))
@@ -10,5 +11,6 @@ def callback(data):
 
 if __name__ == '__main__':
     rospy.init_node('translate')
-    rospy.Subscriber('turtle_control/turtle_cmd',TurtleVelocity,callback)
+    rospy.Subscriber('turtle_control/turtle_cmd',TurtleVelocity,translate)
+    s = rospy.Service('translate',Empty,translate)
     rospy.spin()
